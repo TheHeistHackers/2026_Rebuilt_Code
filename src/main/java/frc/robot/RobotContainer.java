924b4f9fb3f942f91a1bfa225ef7e8bcd11a845e
Patrik Holba <paveegamedev@gmail.com>
2026-02-14 21:21:01 +0100

    Intake - done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import frc.robot.Constants.OIConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,6 +22,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -49,6 +52,15 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    //BASIC
+    // m_driverController.a().whileTrue(m_intakeSubsystem.runIntake());
+    // m_driverController.x().whileTrue(m_intakeSubsystem.runIntakeReverse());
+
+
+    //Toggle
+    m_driverController.a().toggleOnTrue(m_intakeSubsystem.runIntake());
+    m_driverController.x().whileTrue(m_intakeSubsystem.runIntakeReverse());
   }
 
   /**
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import java.security.cert.X509CRLSelector;
+
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
@@ -11,6 +13,7 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -39,17 +42,58 @@ public class IntakeSubsystem extends SubsystemBase {
     SparkMaxConfig intakeConfig = new SparkMaxConfig();
 
     //  Find the needed parameters
+    // I added the most common ones that all FRC teams use, but the values may need to change
+
     //Set the brake for when not receiving a command
     intakeConfig.idleMode(IdleMode.kBrake);
 
+    //Smart cutoff, the motor will try to keep this amount of Amperes
     intakeConfig.smartCurrentLimit(35);
+    //The main cutoff - it will never go above it
     intakeConfig.secondaryCurrentLimit(50);
+    //Stabilizes the torgue so that when the battery is fresh it works the same way as if it's not
     intakeConfig.voltageCompensation(12.0);
 
 
     //This line makes sure the settings are persistant and that we reset the controller to safe defaults
     intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
+  }
+
+  //Most basic
+
+  // public Command runIntake(){
+  //   return run(() -> intakeMotor.set(1));
+  // }
+
+  // public Command runIntakeReverse(){
+  //   return run(() -> intakeMotor.set(-1));
+  // }
+
+
+  //With voltage
+
+  // public Command runIntake(){
+  //   return run(() -> intakeMotor.setVoltage(12));
+  // }
+
+  // public Command runIntakeReverse(){
+  //   return run(() -> intakeMotor.setVoltage(-12));
+  // }
+
+
+  //With toggles
+
+  public Command runIntake(){
+    return runEnd(
+        () -> intakeMotor.setVoltage(12),
+        () -> intakeMotor.setVoltage(0));
+  }
+
+  public Command runIntakeReverse(){
+    return runEnd(
+        () -> intakeMotor.setVoltage(-12),
+        () -> intakeMotor.setVoltage(0)); //technically we don't need this
   }
 
   @Override
